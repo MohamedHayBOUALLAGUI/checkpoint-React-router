574eae0041d498914f9a15a2a42571bc84fce2bd
MohamedHayBOUALLAGUI <haithem.bouallagui90@gmail.com>
2020-11-09 18:56:32 +0100

    checkpoint react router

---
+++ src/components/MoviesData.js
@@ -0,0 +1,114 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const moviesData = [
+  {
+    id: uuidv4(),
+    title: "JOKER",
+    description:
+      "In Gotham City, mentally troubled comedian Arthur Fleck is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego: the Joker.",
+    posterUrl:
+      "https://m.media-amazon.com/images/M/MV5BNGVjNWI4ZGUtNzE0MS00YTJmLWE0ZDctN2ZiYTk2YmI3NTYyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    date: 2019,
+    rate: 5,
+    trailerUrl:"https://www.youtube.com/embed/zAGVQLHvwOY"
+  },
+  {
+    id: uuidv4(),
+    title: "Parasite",
+    description:
+      "Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.",
+    posterUrl:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_UY209_CR0,0,140,209_AL_.jpg",
+    date: 2019,
+    rate: 5,
+    trailerUrl:"https://www.youtube.com/embed/5xH0HfJHsaY"
+  },
+  {
+    id: uuidv4(),
+    title: "John Wick Parabellum",
+    description:
+      "John Wick is on the run after killing a member of the international assassins' guild, and with a $14 million price tag on his head, he is the target of hit men and women everywhere.",
+    posterUrl:
+      "https://m.media-amazon.com/images/M/MV5BMDg2YzI0ODctYjliMy00NTU0LTkxODYtYTNkNjQwMzVmOTcxXkEyXkFqcGdeQXVyNjg2NjQwMDQ@._V1_UX140_CR0,0,140,209_AL_.jpg",
+    date: 2019,
+    rate: 4,
+    trailerUrl:"https://www.youtube.com/embed/M7XM597XO94"
+  },
+  {
+    id: uuidv4(),
+    title: "El Camino",
+    description:
+      "Fugitive Jesse Pinkman runs from his captors, the law, and his past.",
+    posterUrl:
+      "https://m.media-amazon.com/images/M/MV5BNjk4MzVlM2UtZGM0ZC00M2M1LThkMWEtZjUyN2U2ZTc0NmM5XkEyXkFqcGdeQXVyOTAzMTc2MjA@._V1_UY209_CR13,0,140,209_AL_.jpg",
+    date: 2019,
+    rate: 4,
+    trailerUrl:"https://www.youtube.com/embed/1JLUn2DFW4w"
+  },
+  {
+    id: uuidv4(),
+    title: "The Lion King",
+    description:
+      "After the murder of his father, a young lion prince flees his kingdom only to learn the true meaning of responsibility and bravery.",
+    posterUrl:
+      "https://m.media-amazon.com/images/M/MV5BMjIwMjE1Nzc4NV5BMl5BanBnXkFtZTgwNDg4OTA1NzM@._V1_UY209_CR0,0,140,209_AL_.jpg",
+    date: 2019,
+    rate: 3,
+    trailerUrl:"https://www.youtube.com/embed/7TavVZMewpY"
+  },
+  {
+    id: uuidv4(),
+    title: "The Dirt",
+    description:
+      "Based on the bestselling autobiography from Mötley Crüe, the film is an unflinching tale of success and excess as four misfits rise from the streets of Hollywood to the heights of international fame",
+    posterUrl:
+      "https://m.media-amazon.com/images/M/MV5BODhiMzkwYTctYzgwOC00MDM2LWExYjQtMzY4MDljZjQ3M2RmXkEyXkFqcGdeQXVyNDg4NjY5OTQ@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    date: 2019,
+    rate: 3,
+    trailerUrl:"https://www.youtube.com/embed/-NOp5ROn1HE"
+  },
+  {
+    id: uuidv4(),
+    title: "Aladdin",
+    description:
+      "A kind-hearted street urchin and a power-hungry Grand Vizier vie for a magic lamp that has the power to make their deepest wishes come true.",
+    posterUrl:
+      "https://m.media-amazon.com/images/M/MV5BMjQ2ODIyMjY4MF5BMl5BanBnXkFtZTgwNzY4ODI2NzM@._V1_UY209_CR0,0,140,209_AL_.jpg",
+    date: 2019,
+    rate: 2,
+    trailerUrl:"https://www.youtube.com/embed/foyufD52aog"
+  },
+  {
+    id: uuidv4(),
+    title: "Cold War",
+    description:
+      "In the 1950s, a music director falls in love with a singer and tries to persuade her to flee communist Poland for France.",
+    posterUrl:
+      "https://m.media-amazon.com/images/M/MV5BNTJmNzExOGItZTQyMi00YzBlLTk0ZTQtNzAxYmUwZDQwZjU4XkEyXkFqcGdeQXVyODE1MjMyNzI@._V1_UY209_CR0,0,140,209_AL_.jpg",
+    date: 2018,
+    rate: 2,
+    trailerUrl:"https://www.youtube.com/embed/8ImvkXgGVWw"
+  },
+  {
+    id: uuidv4(),
+    title: "Monster a Go-Go",
+    description:
+      "A space capsule crash-lands on Earth, and the astronaut aboard disappears. Is there a connection between the missing man and the monster roaming the area?",
+    posterUrl:
+      "https://m.media-amazon.com/images/M/MV5BNjY0OWQ0ZDctNDI4ZC00MTc2LThhOWItYmE2NWQwYjg0OWVjXkEyXkFqcGdeQXVyNjE5MjUyOTM@._V1_UX140_CR0,0,140,209_AL_.jpg",
+    date: 1965,
+    rate: 1,
+    trailerUrl:"https://www.youtube.com/embed/w1p3UTqa3d4"
+  },
+  {
+    id: uuidv4(),
+    title: "Birdemic",
+    description:
+      "A horde of mutated birds descends upon the quiet town of Half Moon Bay, California. With the death toll rising, Two citizens manage to fight back, but will they survive Birdemic?",
+    posterUrl: "https://m.media-amazon.com/images/M/MV5BZTMyNmM1ODYtZmU3OS00NWIwLTg0MWEtNDExY2NlMzYzNTQ4XkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR2,0,182,268_AL_.jpg",
+    date: 2010,
+    rate: 1,
+    trailerUrl:"https://www.youtube.com/embed/Yy-9YLpC5uM"
+  },
+];
+
